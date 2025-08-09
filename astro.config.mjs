04bc55ed70384f3e69c325f1ad9a749c73c1e739
Tom Harris <tomharris3@me.com>
2025-08-09 19:12:47 +0100

    reset and add initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: 'https://groovetech.co.uk',
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => page.startsWith('https://groovetech.co.uk/blog'),
+    }),
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
